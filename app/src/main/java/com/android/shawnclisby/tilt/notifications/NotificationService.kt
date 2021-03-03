46fbc9fa3b5e3e75ef261dfb0bec27132919f848
Shawn Clisby <shawn.clisby@gmail.com>
2021-03-03 15:54:50 -0500

    Connected Firebase to project for push notifications.

---
+++ app/src/main/java/com/android/shawnclisby/tilt/notifications/NotificationService.kt
@@ -0,0 +1,11 @@
+package com.android.shawnclisby.tilt.notifications
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class NotificationService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+    }
+}
